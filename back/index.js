2cc7931b352d1c8234ca516766c1cad762c5fcf9
Matheus Mauricio <lhr3@hotmail.com>
2024-06-10 17:16:48 -0300

    implementado inserção e busca das mensagens no banco de dados

--- back/index.js
+++ back/index.js
@@ -2,27 +2,59 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const MessageRepository = require('./repository/message');
+const MessageService = require('./service/message');
+const Message = require('./models/message')
+
+dotenv.config()
 
 const app = express();
 const port = 3001;
-
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/api/gpt', async (req, res) => {
-    const { prompt } = req.body;
+const databaseConfig = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWD,
+    database: process.env.MYSQL_DATABASE
+};
 
-    const mockResponse = {
-        choices: [
-            {
-                message: {
-                    content: "Configure uma api key válida para utilizar a api da open ai"
-                }
+function create_table(databaseConfig){
+    const mysql = require('mysql2')
+
+    const connection = mysql.createConnection(databaseConfig);
+    const createTableSql = `
+            CREATE TABLE IF NOT EXISTS messages (
+                text VARCHAR(140),
+                usertype VARCHAR(10),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `;
+    connection.query(createTableSql, (err, result) => {
+        if (err) {
+            console.error('Error creating messages table:', err);
+            throw err;
+        }
+        console.log('Messages table created or already exists');
+
+        connection.end((err) => {
+        if (err) {
+            console.error('Error closing the database connection:', err);
+            } else {
+                console.log('Database connection closed');
             }
-        ]
-    };
+        });
+    });
+}
 
-    /*
+create_table(databaseConfig);
+
+const message_service = new MessageService(new MessageRepository(databaseConfig))
+
+async function callGPT(prompt){
     try {
         const apiUrl = 'https://api.openai.com/v1/chat/completions';
         const payload = {
@@ -33,16 +65,76 @@ app.post('/api/gpt', async (req, res) => {
         };
         const response = await axios.post(apiUrl, payload, {
             headers: {
-                'Authorization': '',
+                'Authorization': 'Bearer sk-proj-CYzQm2pHM65EAthVF0QXT3BlbkFJ5gCUjYSDBiq2ET3287tM',
                 'Content-Type': 'application/json'
             }
         }); 
+        return response.data
     } catch (error) {
         console.error('Erro ao solicitar ao GPT:', error);
-        res.status(500).send('Erro ao solicitar ao GPT');
+        throw error
     }
-    */
-    res.json(mockResponse);
+}
+
+function error_response(error){
+    const response = { choices: [
+         {
+             message: {
+                 content: error
+             }
+         }
+     ]
+ }
+ return response
+}
+
+app.post('/api/message', async (req, res) => {
+    const { prompt } = req.body;
+
+    if (!prompt) {
+        return res.status(422).json(error_response('Invalid Payload parameters'));
+    }
+
+    try {
+        const new_message = await message_service.create_message(
+            new Message(prompt, 'user')
+        );
+    } catch (err) {
+        return res.status(422).json(error_response('An error occurred while creating message'));
+    }
+
+    let user_answer;
+    try {
+        user_answer = await callGPT(prompt);
+        /*user_answer = {
+            choices:[{
+                message:{
+                    content:"Nao"
+                }
+            }]
+        }*/
+    } catch (err) {
+        return res.status(429).json(error_response(err.response.data.error.message));
+    }
+
+    try {
+        const gpt_response = await message_service.create_message(
+            new Message(user_answer.choices[0].message.content, 'assistant')
+        );
+    } catch (err) {
+        return res.status(500).json(error_response('An error occurred while creating GPT response'));
+    }
+
+    return res.status(201).json(user_answer);
+});
+
+app.get('/api/message', async (req, res) => {
+    try {
+        const messages = await message_service.get_all_messages();
+        return res.status(200).json(messages);
+    } catch (err) {
+        return res.status(500).json(error_response('An error occurred when searching for messages'));
+    }
 });
 
 app.listen(port, () => {
